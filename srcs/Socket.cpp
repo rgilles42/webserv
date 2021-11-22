77cf877a6a0e8525c708da684614769733d9669d
rgilles <rgilles@student.42.fr>
2021-11-22 18:35:51 +0100

    adding accept methof

--- srcs/Socket.cpp
+++ srcs/Socket.cpp
@@ -6,14 +6,14 @@
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:51:02 by rgilles           #+#    #+#             */
-/*   Updated: 2021/11/22 15:24:35 by rgilles          ###   ########.fr       */
+/*   Updated: 2021/11/22 18:28:48 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/Socket.hpp"
 
-Socket::Socket() : _fd(-1) {}
+Socket::Socket() : _fd(-1), _len(sizeof(struct sockaddr)) {}
 
-Socket::Socket(const char* addr, unsigned short port) : _fd(socket(AF_INET, SOCK_STREAM, 0))
+Socket::Socket(const char* addr, unsigned short port) : _fd(socket(AF_INET, SOCK_STREAM, 0)), _len(sizeof(struct sockaddr))
 {
 	int					optval = 1;
 	struct sockaddr_in&	address = reinterpret_cast<struct sockaddr_in&>(this->_addr);
@@ -25,7 +25,6 @@ Socket::Socket(const char* addr, unsigned short port) : _fd(socket(AF_INET, SOCK
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr(addr);
 	address.sin_port = htons(port);
-	this->_len = sizeof(this->_addr);
 }
 
 Socket::Socket(const Socket& src) {*this = src;}
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:47:40 by ppaglier          #+#    #+#             */
-/*   Updated: 2021/11/22 18:15:35 by rgilles          ###   ########.fr       */
+/*   Updated: 2021/11/22 18:32:35 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ ssize_t receive_basic(SOCKET s, std::string &result)
 	// 	}
 	// 	result.append(buffer);
 	// }
-	size_read = recv(s, buffer, BUFFER_SIZE + 1, 0);
+	size_read = read(s, buffer, BUFFER_SIZE + 1);
 	buffer[size_read] = '\0';
 	result.append(buffer);
 	return result.length();
@@ -61,9 +61,9 @@ int main(void) {
 
 	//SOCKET server_socket;
 	//SOCKADDR_IN server_addr;
-	// SOCKET client_socket;
-	SOCKADDR_IN client_addr;
-	socklen_t recsize = sizeof(client_addr);
+	// SOCKET ClientSocket;
+	//SOCKADDR_IN client_addr;
+	//socklen_t recsize = sizeof(client_addr);
 	std::string message;
 
 	/*if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -75,7 +75,8 @@ int main(void) {
 		perror("setsockopt(SO_REUSEADDR) failed");
 		return EXIT_FAILURE;
 	}*/
-	Socket	ServerSocket("0.0.0.0", 80);
+	Socket	ServerSocket("0.0.0.0", 8000);
+	Socket	ClientSocket;
 
 //	/* Configuration */
 //	server_addr.sin_addr.s_addr	= htonl(INADDR_ANY);   /* Adresse IP automatique */
@@ -87,7 +88,9 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 
-	printf("La socket %d est maintenant ouverte en mode TCP/IP (%d:%d)\n", ServerSocket.fd(), ntohl(((struct sockaddr_in*)&ServerSocket.addr())->sin_addr.s_addr), ntohs(((struct sockaddr_in*)&ServerSocket.addr())->sin_port));
+	printf("La socket %d est maintenant ouverte en mode TCP/IP (%d:%d)\n",
+		ServerSocket.fd(), ntohl(((struct sockaddr_in*)&ServerSocket.addr())->sin_addr.s_addr),
+		ntohs(((struct sockaddr_in*)&ServerSocket.addr())->sin_port));
 
 
 	/* Démarrage du listage (mode server) */
@@ -99,27 +102,29 @@ int main(void) {
 	printf("Listage du port %d...\n", ntohs(((struct sockaddr_in*)&ServerSocket.addr())->sin_port));
 
 	/* Attente pendant laquelle le client se connecte */
-	printf("Patientez pendant que le client se connecte sur le port %d...\n", ntohs(((struct sockaddr_in*)&ServerSocket.addr())->sin_port));
+	printf("Patientez pendant que le client se connecte sur le port %d...\n",
+		ntohs(((struct sockaddr_in*)&ServerSocket.addr())->sin_port));
 
 	signal(SIGINT, handleSignals);
 	while (!stop) {
 
 
-		SOCKET client_socket = accept(ServerSocket.fd(), (SOCKADDR*)&client_addr, &recsize);
-		printf("Un client se connecte avec la socket %d de %s:%d\n", client_socket, inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port));
+		ClientSocket = ServerSocket.accept();
+		printf("Un client se connecte avec la socket %d de %s:%d\n",
+			ClientSocket.fd(), inet_ntoa(((struct sockaddr_in*)&ClientSocket.addr())->sin_addr), htons(((struct sockaddr_in*)&ClientSocket.addr())->sin_port));
 
-		// if(!(fcntl(client_socket, F_GETFL) & O_NONBLOCK)) {
-		// 	if(fcntl(client_socket, F_SETFL, fcntl(client_socket, F_GETFL) | O_NONBLOCK) < 0) {
+		// if(!(fcntl(ClientSocket.fd(), F_GETFL) & O_NONBLOCK)) {
+		// 	if(fcntl(ClientSocket.fd(), F_SETFL, fcntl(ClientSocket.fd(), F_GETFL) | O_NONBLOCK) < 0) {
 
 		// 	}
 		// }
 		message.clear();
-		receive_basic(client_socket, message);
+		receive_basic(ClientSocket.fd(), message);
 
 		// if (sock_err < 0) {
 		// 	printf("error de récéption\n");
 		// } else {
-		// 	printf("Chaine reçu : %s\n", message.c_str());
+		printf("Chaine reçue : %s\n", message.c_str());
 		// }
 
 		HttpRequest request(message);
@@ -131,7 +136,7 @@ int main(void) {
 
 		HttpResponse response(currentRessource);
 
-		send(client_socket, response.toString().c_str(), response.toString().length(), 0);
+		send(ClientSocket.fd(), response.toString().c_str(), response.toString().length(), 0);
 
 		// if (sock_err < 0) {
 		// 	printf("error de transmission\n");
@@ -140,8 +145,7 @@ int main(void) {
 		// }
 
 		/* Il ne faut pas oublier de fermer la connexion (fermée dans les deux sens) */
-		shutdown(client_socket, 2);
-		closesocket(client_socket);
+		shutdown(ClientSocket.fd(), 2);
 	}
 
 	/* Fermeture de la socket */
@@ -156,41 +160,3 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
-// int main(void) {
-// 	std::cout << "|" << HttpStatusCode::getStatusCodeString(HttpStatusCode::unknown) << "|" << std::endl;
-// 	std::cout << "|" << HttpStatusCode::getStatusCodeString(HttpStatusCode::information_continue) << "|" << std::endl;
-
-// 	HttpStatusCode::StatusCode code;
-
-// 	code = HttpStatusCode::getStatusCode(200);
-// 	std::cout << "|" << HttpStatusCode::getStatusCodeString(code) << "|" << std::endl;
-// 	code = HttpStatusCode::getStatusCode(201);
-// 	std::cout << "|" << HttpStatusCode::getStatusCodeString(code) << "|" << std::endl;
-// 	code = HttpStatusCode::getStatusCode(189);
-// 	std::cout << "|" << HttpStatusCode::getStatusCodeString(code) << "|" << std::endl;
-// 	code = HttpStatusCode::getStatusCode("502 Bad Gateway");
-// 	std::cout << "|" << HttpStatusCode::getStatusCodeString(code) << "|" << std::endl;
-
-// std::cout << "|" << HttpVersion::getVersion("") << "|" << std::endl;
-// std::cout << "|" << HttpVersion::getVersion("HTTP/0.9") << "|" << std::endl;
-// std::cout << "|" << HttpVersion::getVersion("HTTP/1.0") << "|" << std::endl;
-// std::cout << "|" << HttpVersion::getVersion("HTTP/1.1") << "|" << std::endl;
-// std::cout << "|" << HttpVersion::getVersion("HTTP/1.2") << "|" << std::endl;
-// std::cout << "|" << HttpVersion::getVersion("HTTP/2") << "|" << std::endl;
-// std::cout << "|" << HttpVersion::getVersion("HTTP/3") << "|" << std::endl;
-
-// 	return EXIT_SUCCESS;
-// }
-
-// int main(void) {
-// 	std::string message = "GET /test?oui=non HTTP/1.1\r\nHost: 127.0.0.1\r\nhost: 128.0.0.1\r\nHost: 129.0.0.1\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nsec-ch-ua: \"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: \"Windows\"\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: cross-site\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7\r\nIf-Modified-Since: Tue, 11 Nov 2021 19:20:25 GMT\r\n\r\n";
-// 	HttpRequest request(message);
-
-// 	std::cout << request.get("Host") << std::endl;
-// 	std::cout << request.get("host") << std::endl;
-// 	std::cout << request.get("Host") << std::endl;
-// 	std::cout << request.get("Connection") << std::endl;
-// 	std::cout << request.get("Cache-Control") << std::endl;
-
-// 	return EXIT_SUCCESS;
-// }
