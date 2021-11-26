15feca4e0ce1749907a1aef472cdb3ace7aec8de
ppaglier <ppaglier@student.42.fr>
2021-11-26 17:47:43 +0100

    Add defult methods for Server & Route

--- includes/http/Route.hpp
+++ includes/http/Route.hpp
@@ -6,7 +6,7 @@
 /*   By: ppaglier <ppaglier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 14:03:05 by ppaglier          #+#    #+#             */
-/*   Updated: 2021/11/26 17:17:16 by ppaglier         ###   ########.fr       */
+/*   Updated: 2021/11/26 17:45:25 by ppaglier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,62 @@ namespace Webserv {
 			public:
 				Route(void);
 				~Route();
-		};
 
+				void	setErrorPage(const error_pages_type::key_type &errorCode, const error_pages_type::mapped_type &page);
+				void	setClientMaxBodySize(const client_max_body_size_type &size = client_max_body_size_type(1, client_max_body_size_type::U_MB));
+				void	setUploadStore(const upload_store_type &upload_store = "/");
+
+				void	setReturn(const return_type &_return);
+				void	setAutoindex(const autoindex_type &autoindex);
+				void	setRoot(const root_type &root);
+				void	setIndex(const index_type &index);
+
+				void	setLimitExcept(const limit_except_type &limit_except);
+
+				void	addRoute(const routes_map::key_type &path, const routes_map::mapped_type &route);
+
+		};
 
 		Route::Route(void) {}
 
 		Route::~Route() {}
 
+		void 	Route::setErrorPage(const error_pages_type::key_type &errorCode, const error_pages_type::mapped_type &page) {
+			this->error_pages[errorCode] = page;
+		}
+
+		void 	Route::setClientMaxBodySize(const client_max_body_size_type &client_max_body_size) {
+			this->client_max_body_size = client_max_body_size;
+		}
+
+		void 	Route::setUploadStore(const upload_store_type &upload_store) {
+			this->upload_store = upload_store;
+		}
+
+		void 	Route::setReturn(const return_type &_return) {
+			this->_return = _return;
+		}
+
+
+		void 	Route::setAutoindex(const autoindex_type &autoindex) {
+			this->autoindex = autoindex;
+		}
+
+		void 	Route::setRoot(const root_type &root) {
+			this->root = root;
+		}
+
+		void 	Route::setIndex(const index_type &index) {
+			this->index = index;
+		}
+
+		void 	Route::setLimitExcept(const limit_except_type &limit_except) {
+			this->limit_except = limit_except;
+		}
+
+		void 	Route::addRoute(const routes_map::key_type &path, const routes_map::mapped_type &route) {
+			this->routes[path] = route;
+		}
 
 	} // namespace Http
 
--- includes/http/Server.hpp
+++ includes/http/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: ppaglier <ppaglier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 14:03:05 by ppaglier          #+#    #+#             */
-/*   Updated: 2021/11/26 17:16:56 by ppaglier         ###   ########.fr       */
+/*   Updated: 2021/11/26 17:44:07 by ppaglier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,16 @@ namespace Webserv {
 				void	setPort(const port_type &port = "80");
 
 				void	setErrorPage(const error_pages_type::key_type &errorCode, const error_pages_type::mapped_type &page);
-				void	setClientMaxBodySize(const client_max_body_size_type &size = client_max_body_size_type(1, client_max_body_size_type::U_MB));
+				void	setClientMaxBodySize(const client_max_body_size_type &client_max_body_size = client_max_body_size_type(1, client_max_body_size_type::U_MB));
+				void	setUploadStore(const upload_store_type &upload_store = "/");
+
+				void	setReturn(const return_type &_return);
+				void	setAutoindex(const autoindex_type &autoindex);
+				void	setRoot(const root_type &root);
+				void	setIndex(const index_type &index);
+
+				void	addRoute(const routes_map::key_type &path, const routes_map::mapped_type &route);
+
 		};
 
 
@@ -85,6 +94,52 @@ namespace Webserv {
 
 		Server::~Server() {}
 
+		void 	Server::setServerName(const name_type &serverName) {
+			this->serverName = serverName;
+		}
+
+		void 	Server::setHost(const host_type &host) {
+			this->host = host;
+		}
+
+		void 	Server::setPort(const port_type &port) {
+			this->port = port;
+		}
+
+
+		void 	Server::setErrorPage(const error_pages_type::key_type &errorCode, const error_pages_type::mapped_type &page) {
+			this->error_pages[errorCode] = page;
+		}
+
+		void 	Server::setClientMaxBodySize(const client_max_body_size_type &client_max_body_size) {
+			this->client_max_body_size = client_max_body_size;
+		}
+
+		void 	Server::setUploadStore(const upload_store_type &upload_store) {
+			this->upload_store = upload_store;
+		}
+
+		void 	Server::setReturn(const return_type &_return) {
+			this->_return = _return;
+		}
+
+
+		void 	Server::setAutoindex(const autoindex_type &autoindex) {
+			this->autoindex = autoindex;
+		}
+
+		void 	Server::setRoot(const root_type &root) {
+			this->root = root;
+		}
+
+		void 	Server::setIndex(const index_type &index) {
+			this->index = index;
+		}
+
+
+		void 	Server::addRoute(const routes_map::key_type &path, const routes_map::mapped_type &route) {
+			this->routes[path] = route;
+		}
 
 	} // namespace Http
 
